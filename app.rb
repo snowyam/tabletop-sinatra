6defb0c9457bf7fc7eeea834cdf6ade33e1a762d
Sy <syeung@mail.usf.edu>
2016-07-29 23:36:03 -0400

    created skeleton layout, css, routes

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 class Tabletop < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  not_found do
+    erb :error
+  end
+
   get '/' do
-    "Hello, World!"
+    erb :index
+  end
+
+  get '/guesser' do
+    erb :guesser
+  end
+
+  get '/caesar' do
+    erb :caesar
+  end
+
+  get '/hangman' do
+    erb :hangman
   end
 end
